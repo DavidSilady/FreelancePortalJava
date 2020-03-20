a25a6e1584330856c3213477e85f389601c3c7e3
DavidSilady <david.silady@gmail.com>
2020-03-20 21:07:44 +0100

    Moved Main to its own package

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("template/registration.fxml"));
+        primaryStage.setTitle("Freelance Portal");
+        primaryStage.setScene(new Scene(root, 680, 720));
         primaryStage.show();
     }
 
