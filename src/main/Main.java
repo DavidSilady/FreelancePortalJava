c3933c19a1c77a7706e487a3860bfd81ae98b8bc
DavidSilady <david.silady@gmail.com>
2020-03-20 22:19:56 +0100

    Added registration.fxml => cannot start in JDK 14 => migrating to JDK 11.0.2

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("template/registration.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/template/registration.fxml"));
         primaryStage.setTitle("Freelance Portal");
         primaryStage.setScene(new Scene(root, 720, 540));
         primaryStage.show();
