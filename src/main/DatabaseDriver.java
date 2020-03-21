f440493da203dabe92b38b7df7e9567bdae024a8
Adam Rybansky <adam.rybansky@centrum.sk>
2020-03-21 16:56:06 +0100

    Added connection to database

---
+++ src/main/DatabaseDriver.java
@@ -0,0 +1,44 @@
+package main;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
+public class DatabaseDriver {
+
+    static final String JDBC_DRIVER = "org.postgresql.Driver";
+    static final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
+    static final String USER = "postgres";
+    static final String PASS = "houno";
+
+    public static void insert_user(List<String> userData) {
+
+        try {
+            Connection connection = null;
+            Statement statement = null;
+
+            Class.forName(JDBC_DRIVER);
+            connection = DriverManager
+                    .getConnection(DB_URL, USER, PASS);
+            connection.setAutoCommit(false);
+            statement = connection.createStatement();
+
+            SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+            String sql_code = "INSERT INTO USERS (name, surname, mail, password, registration_date) " +
+                    "VALUES ( '" + userData.get(0) + "','" + userData.get(1) + "','" + userData.get(2) + "','" + userData.get(3) + "','" +
+            formatter.parse(userData.get(4)) + "')";
+            System.out.println(sql_code);
+
+            statement.executeUpdate(sql_code);
+            statement.close();
+            connection.commit();
+            connection.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+    }
+}
