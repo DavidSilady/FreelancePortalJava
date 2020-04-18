2f5a8766e456183fd3765fe3551a72677984597d
DavidSilady <david.silady@gmail.com>
2020-04-18 15:36:07 +0200

    Added listing subpage

---
+++ src/controller/GigListingController.java
@@ -0,0 +1,38 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class GigListingController {
+	
+	@FXML
+	private JFXButton detailButton;
+	
+	@FXML
+	private JFXButton freelancerProfileButton;
+	
+	@FXML
+	private Label gigNameLabel;
+	
+	@FXML
+	private Label gigDescription;
+	
+	@FXML
+	private Label ratingLabel;
+	
+	@FXML
+	private Label freelancerAliasLabel;
+	
+	@FXML
+	void showFreelancerProfile(ActionEvent event) {
+	
+	}
+	
+	@FXML
+	void showGigDetail(ActionEvent event) {
+	
+	}
+	
+}
