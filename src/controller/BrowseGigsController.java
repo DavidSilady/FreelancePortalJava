db16a229e8deaa68e0ae61fbb60f2a7730a74bfc
DavidSilady <david.silady@gmail.com>
2020-04-19 13:46:58 +0200

    Added dynamic filtering

--- src/controller/BrowseGigsController.java
+++ src/controller/BrowseGigsController.java
@@ -2,10 +2,12 @@ package controller;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import model.DatabaseDriver;
 import model.Gig;
@@ -21,9 +23,6 @@ public class BrowseGigsController {
     
     @FXML
     private JFXComboBox<String> sortComboBox;
-    
-    @FXML
-    private JFXButton PurchaseSelectedButton;
 
     @FXML
     private JFXComboBox<String> categoryComboBox;
@@ -32,14 +31,14 @@ public class BrowseGigsController {
     private Label label1;
 
     @FXML
-    private JFXButton ConfirmButton;
+    private JFXButton filterButton;
 
     private User currentUser;
     
     private int pageNum;
     
     @FXML
-    private Label pageNumLabel;
+    private JFXTextField pageNumField;
     
     @FXML
     private JFXButton rightButton;
@@ -50,59 +49,100 @@ public class BrowseGigsController {
     @FXML
     private AnchorPane listingPane;
     
+    @FXML
+    private JFXTextField filterNameField;
+    
     private ListingContainer listingController;
 
+    private String categoryConditionStatement;
+    private String orderByStatement;
+    private String filterNameStatement;
+    
     @FXML
-    void purchaseSelected(ActionEvent event) {
-
+    void updateFilter(KeyEvent event) throws Exception {
+        filterNameStatement = generateFilterNameStatement();
+        updateGigTable();
+    }
+    /*
+    where LOWER(alias) like LOWER('%va%')
+    OR LOWER(g.gig_name) like LOWER('%va%');
+     */
+    
+    private String generateFilterNameStatement () {
+        String filterText = filterNameField.getText();
+        if (filterText.isEmpty())
+            return "";
+        String[] splitString = filterText.split("\\s+");
+        StringBuilder statement = new StringBuilder("WHERE ");
+        for (String word: splitString) {
+            statement.append("LOWER(g.gig_name) LIKE LOWER('%").append(word);
+            statement.append("%') OR LOWER(f.alias) LIKE LOWER('%").append(word).append("%')\n");
+            statement.append("OR ");
+        }
+        statement.delete(statement.length() - 3, statement.length());
+        return statement.toString();
+    }
+    
+    @FXML
+    void changePage(ActionEvent event) throws Exception {
+        try {
+            pageNum = Integer.parseInt(pageNumField.getText());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        updateGigTable();
+    }
+    
+    
+    @FXML
+    void reset(ActionEvent event) throws Exception {
+        categoryComboBox.setValue("");
+        sortComboBox.setValue("Most Popular");
+        categoryConditionStatement = generateCategoryConditionStatement();
+        orderByStatement = generateOrderByStatement();
+        pageNum = 1;
+        updateGigTable();
     }
     
     private void updateGigTable() throws Exception {
-        if ( categoryComboBox.getSelectionModel().isEmpty()) {
-            ArrayList<Listable> gigs = fetchMostPopularGigs();
-            listingController.updateListing(gigs);
-        } else {
-            String category = categoryComboBox.getValue();
-            ArrayList<Listable> gigs = currentUser.findGigByCategory(category, pageNum);
-            listingController.updateListing(gigs);
-        }
-        
+        ArrayList<Listable> gigs = fetchGigs();
+        listingController.updateListing(gigs);
     }
     
     @FXML
     void updateNext(ActionEvent event) throws Exception {
         pageNum += 1;
-        pageNumLabel.setText(String.valueOf(pageNum));
+        pageNumField.setText(String.valueOf(pageNum));
         updateGigTable();
     }
     
     @FXML
     void updatePrevious(ActionEvent event) throws Exception {
         pageNum -= 1;
-        if (pageNum < 0) pageNum = 0;
-        pageNumLabel.setText(String.valueOf(pageNum));
+        if (pageNum < 1) pageNum = 1;
+        pageNumField.setText(String.valueOf(pageNum));
         updateGigTable();
     }
     
     
-
     @FXML
-    void updateByCategory (ActionEvent event) throws Exception {
-        if ( categoryComboBox.getSelectionModel().isEmpty()) return;
-        pageNum = 0;
-        pageNumLabel.setText(String.valueOf(pageNum+1));
-        String category = categoryComboBox.getValue();
-        ArrayList<Listable> gigs = currentUser.findGigByCategory(category, pageNum);
-        listingController.updateListing(gigs);
+    void filter (ActionEvent event) throws Exception {
+        categoryConditionStatement = generateCategoryConditionStatement();
+        orderByStatement = generateOrderByStatement();
+        pageNum = 1;
+        updateGigTable();
     }
 
     public void init(User currentUser) throws Exception {
-        pageNum = 0;
-        pageNumLabel.setText(String.valueOf(pageNum+1));
+        pageNum = 1;
+        pageNumField.setText(String.valueOf(pageNum));
         this.currentUser = currentUser;
         categoryComboBox.getItems().addAll(currentUser.getAllCategories());
-        sortComboBox.getItems().add("Most Popular");
-        ArrayList<Listable> gigs = fetchMostPopularGigs();
+        setupSortComboBox();
+        categoryConditionStatement = generateCategoryConditionStatement();
+        orderByStatement = generateOrderByStatement();
+        filterNameStatement = "";
+        ArrayList<Listable> gigs = fetchGigs();
         SceneManager sceneManager = new SceneManager();
         FXMLLoader fxmlLoader = sceneManager.switchDynamicPane(listingPane, "listingContainer");
         ListingContainer controller = fxmlLoader.getController();
@@ -110,16 +150,54 @@ public class BrowseGigsController {
         listingController = controller;
     }
     
-    private ArrayList<Listable> fetchMostPopularGigs() {
+    private void setupSortComboBox() {
+        sortComboBox.getItems().add("Most Popular");
+        sortComboBox.getItems().add("Least Popular");
+        sortComboBox.getItems().add("Best Rating");
+        sortComboBox.getItems().add("Worst Rating");
+        sortComboBox.getItems().add("Most Sold");
+        sortComboBox.getItems().add("Least Sold");
+        sortComboBox.setValue("Most Popular");
+    }
+    
+    //    "ORDER BY ratio DESC\n" +
+    private String generateOrderByStatement () {
+        String orderBy = sortComboBox.getValue();
+        System.out.print(orderBy);
+        if (orderBy.equals("Most Popular"))
+            return "ORDER BY ratio DESC";
+        if (orderBy.equals("Least Popular"))
+            return "ORDER BY ratio";
+        if (orderBy.equals("Best Rating"))
+            return "ORDER BY avg_rating DESC";
+        if (orderBy.equals("Worst Rating"))
+            return "ORDER BY avg_rating";
+        if (orderBy.equals("Most Sold"))
+            return "ORDER BY num_sold DESC";
+        if (orderBy.equals("Least Sold"))
+            return "ORDER BY num_sold";
+        return "ORDER BY ratio DESC";
+    }
+    
+    private String generateCategoryConditionStatement () {
+        if ( categoryComboBox.getSelectionModel().isEmpty())
+            return "";
+        String category = categoryComboBox.getValue();
+        return "WHERE c.category_name = '" + category + "'";
+    }
+    
+    private ArrayList<Listable> fetchGigs () {
         String query = "SELECT g.id, f.freelance_id, c.category_name, g.gig_name, f.alias, " +
                 "COUNT(s.id) AS num_sold, AVG(r.rating) AS avg_rating, AVG(r.rating) * COUNT(s.id) AS ratio FROM gigs AS g\n" +
                 "INNER JOIN categories c ON g.category_id = c.id\n" +
                 "INNER JOIN freelancers f ON g.freelancer_id = f.freelance_id\n" +
                 "INNER JOIN reviews r ON g.id = r.gig_id\n" +
                 "INNER JOIN services s ON g.id = s.gig_id\n" +
+                filterNameStatement +
+                categoryConditionStatement + "\n" +
                 "GROUP BY g.id, g.gig_name, c.category_name, f.freelance_id, f.alias\n" +
-                "ORDER BY ratio DESC\n" +
-                "LIMIT 10 OFFSET " + pageNum * 10;
+                orderByStatement + "\n"+
+                "LIMIT 10 OFFSET " + (pageNum - 1) * 10;
         ArrayList<ArrayList<String>> result = DatabaseDriver.executeQuery(query);
         ArrayList<Listable> gigs = new ArrayList<>();
         for (ArrayList<String> row: Objects.requireNonNull(result)) {
