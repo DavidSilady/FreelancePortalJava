b284947444022d56dd6bd3e429c54ade3eade863
DavidSilady <david.silady@gmail.com>
2020-04-21 13:56:30 +0200

    Added fxml and controller skeleton

---
+++ src/controller/ReviewListingController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class ReviewListingController {
+	
+	@FXML
+	private Label customerNameLabel;
+	
+	@FXML
+	private Label reviewText;
+	
+	@FXML
+	private Label ratingLabel;
+	
+}
