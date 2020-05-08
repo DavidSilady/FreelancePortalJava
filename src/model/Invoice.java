8f44730c1cb38365d838871e26eed1c1295be219
DavidSilady <david.silady@gmail.com>
2020-05-08 15:58:52 +0200

    Creating order process

---
+++ src/model/Invoice.java
@@ -0,0 +1,46 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Invoice {
+	private int id;
+	private String date;
+	private String billingAddress;
+	
+	public int getId () {
+		return id;
+	}
+	
+	public void setId (int id) {
+		this.id = id;
+	}
+	
+	public String getDate () {
+		return date;
+	}
+	
+	public void setDate (String date) {
+		this.date = date;
+	}
+	
+	public String getBillingAddress () {
+		return billingAddress;
+	}
+	
+	public void setBillingAddress (String billingAddress) {
+		this.billingAddress = billingAddress;
+	}
+	
+	public Invoice (String date, String billingAddress) {
+		this.date = date;
+		this.billingAddress = billingAddress;
+	}
+	
+	public void createDBListing() {
+		String query = "INSERT INTO orders (customer_id, order_date) VALUES (1, '" + this.date + "', '" + this.billingAddress + "') RETURNING id";
+		ArrayList<ArrayList<String>> result = DatabaseDriver.executeQuery(query);
+		if (result == null || result.isEmpty())
+			return;
+		this.id = Integer.parseInt(result.get(0).get(0));
+	}
+}
--- src/model/Order.java
+++ src/model/Order.java
@@ -19,7 +19,7 @@ public class Order {
 	}
 	
 	public void createDBListing() {
-		String query = "INSERT INTO orders (customer_id, order_date) VALUES (" + this.customerID + "'" + orderDate + "') RETURNING id";
+		String query = "INSERT INTO orders (customer_id, order_date) VALUES (" + this.customerID + ", '" + orderDate + "') RETURNING id";
 		ArrayList<ArrayList<String>> result = DatabaseDriver.executeQuery(query);
 		if (result == null || result.isEmpty())
 			return;
