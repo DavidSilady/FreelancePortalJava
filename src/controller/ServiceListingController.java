dce5292a442cc01e429cf16e023d1b97f90193dd
DavidSilady <david.silady@gmail.com>
2020-05-08 14:28:52 +0200

    Building service class

--- src/controller/ServiceListingController.java
+++ src/controller/ServiceListingController.java
@@ -1,9 +1,35 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import model.Listable;
 import view.ListablePane;
 
 public class ServiceListingController implements ListablePane {
+	
+	@FXML
+	private JFXTextField descriptionTextField;
+	
+	@FXML
+	private JFXButton updateButton;
+	
+	@FXML
+	private JFXButton deleteButton;
+	
+	@FXML
+	private JFXTextField priceTextField;
+	
+	@FXML
+	void delete(ActionEvent event) {
+	
+	}
+	
+	@FXML
+	void update(ActionEvent event) {
+	
+	}
 	
 	@Override
 	public void init (Listable listing) {
--- src/model/Service.java
+++ src/model/Service.java
@@ -1,8 +1,84 @@
 package model;
 
 public class Service implements Listable {
+	private int id;
+	private int gig_id;
+	private int order_id;
+	private int invoice_id;
+	private int price;
+	private String description;
+	
+	public Service() { }
+	
+	public Service(int gig_id, int price, String description) {
+		this.gig_id = gig_id;
+		this.price = price;
+		this.description = description;
+	}
+	
+	public void setBasicInfo(int gig_id, int price, String description) {
+		this.gig_id = gig_id;
+		this.price = price;
+		this.description = description;
+	}
+	
+	public void setIDs(int order_id, int invoice_id, int gig_id) {
+		this.order_id = order_id;
+		this.gig_id = gig_id;
+		this.invoice_id = invoice_id;
+	}
+	
+	
 	@Override
 	public String getListablePaneName () {
 		return "listings/serviceListing";
+	}
+	
+	public int getId () {
+		return id;
+	}
+	
+	public void setId (int id) {
+		this.id = id;
+	}
+	
+	public int getGig_id () {
+		return gig_id;
+	}
+	
+	public void setGig_id (int gig_id) {
+		this.gig_id = gig_id;
+	}
+	
+	public int getOrder_id () {
+		return order_id;
+	}
+	
+	public void setOrder_id (int order_id) {
+		this.order_id = order_id;
+	}
+	
+	public int getInvoice_id () {
+		return invoice_id;
+	}
+	
+	public void setInvoice_id (int invoice_id) {
+		this.invoice_id = invoice_id;
+	}
+	
+	public int getPrice () {
+		return price;
+	}
+	
+	public void setPrice (int price) {
+		this.price = price;
+	}
+	
+	public String getDescription () {
+		return description;
+	}
+	
+	public void setDescription (String description) {
+		this.description = description;
 	}
 }
