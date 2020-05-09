e7a883ba3b1afd0fa9a75a675b526932cc0ecdd0
DavidSilady <david.silady@gmail.com>
2020-05-09 19:35:27 +0200

    Added mapping for FreelancerORM, GigORM, UserORM

---
+++ src/classesORM/FreelancerORM.java
@@ -0,0 +1,48 @@
+package classesORM;
+
+import javax.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Table(name = "freelancers")
+public class FreelancerORM extends UserORM {
+	@Column(name = "freelance_id", unique = true)
+	private int freelanceId;
+	@Column(name = "alias")
+	private String alias;
+	@Column(name = "description")
+	private String description;
+	
+	public FreelancerORM () {
+	}
+	
+	public FreelancerORM (int freelanceId, String alias, String description) {
+		this.freelanceId = freelanceId;
+		this.alias = alias;
+		this.description = description;
+	}
+	
+	public int getFreelanceId () {
+		return freelanceId;
+	}
+	
+	public void setFreelanceId (int freelanceId) {
+		this.freelanceId = freelanceId;
+	}
+	
+	public String getAlias () {
+		return alias;
+	}
+	
+	public void setAlias (String alias) {
+		this.alias = alias;
+	}
+	
+	public String getDescription () {
+		return description;
+	}
+	
+	public void setDescription (String description) {
+		this.description = description;
+	}
+}
---
+++ src/classesORM/UserORM.java
@@ -0,0 +1,75 @@
+package classesORM;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Table(name = "users")
+public class UserORM {
+	@Id @GeneratedValue
+	private int id;
+	@Column(name = "name")
+	private String name;
+	@Column(name = "surname")
+	private String surname;
+	@Column(name = "email")
+	private String email;
+	@Column(name = "password")
+	private String password;
+	@Column(name = "registration_date")
+	private Date registrationDate;
+	
+	public UserORM () {
+	}
+	
+	
+	
+	public int getId () {
+		return id;
+	}
+	
+	public void setId (int id) {
+		this.id = id;
+	}
+	
+	public String getName () {
+		return name;
+	}
+	
+	public void setName (String name) {
+		this.name = name;
+	}
+	
+	public String getSurname () {
+		return surname;
+	}
+	
+	public void setSurname (String surname) {
+		this.surname = surname;
+	}
+	
+	public String getEmail () {
+		return email;
+	}
+	
+	public void setEmail (String email) {
+		this.email = email;
+	}
+	
+	public String getPassword () {
+		return password;
+	}
+	
+	public void setPassword (String password) {
+		this.password = password;
+	}
+	
+	public Date getRegistrationDate () {
+		return registrationDate;
+	}
+	
+	public void setRegistrationDate (Date registrationDate) {
+		this.registrationDate = registrationDate;
+	}
+}
